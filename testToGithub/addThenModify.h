282fb8223ed354ef647ece9df96202578e6d412e
chendewen <chendewen@17guagua.com>
2016-10-13 18:24:36 +0800

    新增两个文件addThenModify.h，addThenModify.m

---
+++ testToGithub/addThenModify.h
@@ -0,0 +1,13 @@
+//
+//  addThenModify.h
+//  testToGithub
+//
+//  Created by cdw on 16/10/13.
+//  Copyright © 2016年 cdw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface addThenModify : NSObject
+
+@end
---
+++ testToGithub/addThenModify.m
@@ -0,0 +1,13 @@
+//
+//  addThenModify.m
+//  testToGithub
+//
+//  Created by cdw on 16/10/13.
+//  Copyright © 2016年 cdw. All rights reserved.
+//
+
+#import "addThenModify.h"
+
+@implementation addThenModify
+
+@end
